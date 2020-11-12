c22f18fab6e2c45ba7908da643b5a59a238cf5da
gavrysha <alexgavryshua@gmail.com>
2020-11-12 23:19:10 +0200

    Add work with .txt file

--- TestSystem.py
+++ TestSystem.py
@@ -1,18 +1,19 @@
 from tkinter import *
-
-#file = open('input.txt', 'r')
-
+from array import *
+from PIL import ImageTk, Image
+file = open("input.txt", 'r')
+#pip install pillow 
 
 page = Tk()
 page.title("Тестирующая система")
 page.geometry("500x400")
 page.configure(bg = 'grey')
-number = int(input())
+number = int(file.readline().rstrip("\n"))
 typeofans = "1"
 flag = False
-i = 0
+j = 0
 atleastonce = False
-
+numofans = int(0)
 
 nextButton = Button(page, text = "Следующий вопрос", background="#555", foreground="white")
 previousButton = Button(page, text = "Предыдущий вопрос", background="#555", foreground="white")
@@ -21,64 +22,42 @@ previousButton.place(x = 100, y = 300)
 
 
 labelQuestion = Label(page, text = "", font = ('Helvetica', 14, 'bold'))
-while i < number:
+while j < number:
 
 	
-	i += 1
+	j += 1
 
 	active = IntVar();
 	active.set(0);
-
-	Question = str(input())
-	typeofans = str(input())
-	Answer1 = str(input())
-	Answer2 = str(input())
-	Answer3 = str(input())
-	Answer4 = str(input())
-	
-
-	radiobutton1 = Radiobutton(page, text = Answer1, variable = active, value = 0, font = ('Helvetica', 14))
-	radiobutton2 = Radiobutton(page, text = Answer2, variable = active, value = 1, font = ('Helvetica', 14))
-	radiobutton3 = Radiobutton(page, text = Answer3, variable = active, value = 2, font = ('Helvetica', 14))
-	radiobutton4 = Radiobutton(page, text = Answer4, variable = active, value = 3, font = ('Helvetica', 14))
-
-	checkbutton1 = Checkbutton(page, text = Answer1, font = ('Helvetica', 14))
-	checkbutton2 = Checkbutton(page, text = Answer2, font = ('Helvetica', 14))
-	checkbutton3 = Checkbutton(page, text = Answer3, font = ('Helvetica', 14))
-	checkbutton4 = Checkbutton(page, text = Answer4, font = ('Helvetica', 14))
-
-
-	
-
-	
-
-	if typeofans == "1":
-		flag = False
-		if atleastonce == False:
-			atleastonce = True
-		radiobutton1.place(x = 0, y = 50)
-		radiobutton2.place(x = 0, y = 100)
-		radiobutton3.place(x = 0, y = 150)
-		radiobutton4.place(x = 0, y = 200)
-	else:
-		flag = True
-		if atleastonce == False:
-			atleastonce = True
-
-
-		checkbutton1.place(x = 0, y = 50)
-		checkbutton2.place(x = 0, y = 100)
-		checkbutton3.place(x = 0, y = 150)
-		checkbutton4.place(x = 0, y = 200)
+	numofans = int(file.readline().rstrip("\n"))
+	typeofans = str(file.readline().rstrip("\n"))
+	Question = str(file.readline().rstrip("\n"))
+	Question = Question.rstrip("\n")
+	i = 0
+	Answers = []
+	for i in range(numofans):
+		Answers.append(str(file.readline().rstrip("\n")))
+		Answers[i] = Answers[i].rstrip("\n")
+	radiobuttons = []
+	checkbuttons = []
+	images = []
+	i = 0
+	if typeofans == '1':
+		for i in range(numofans):
+			radiobuttons.append(Radiobutton(page, text = Answers[i], variable = active, value = 0, font = ('Helvetica', 14)))
+			radiobuttons[i].place(x = 0, y = 50+i*250/numofans)
+	if typeofans == '2':
+		for i in range(numofans):
+			checkbuttons.append(Checkbutton(page, text = Answers[i], font = ('Helvetica', 14)))
+			checkbuttons[i].place(x = 0, y = 50+i*250/numofans)
+	if typeofans == '3':
+		for i in range(numofans):
+			images.append(ImageTk.PhotoImage(Image.open(Answers[i])))
+			panel = Label(page, image = images[i], width = 10, height = 10)
+			panel.pack(side = LEFT, fill = "both", expand = "yes")
 
 	labelQuestion.config(text=Question)
 
 	labelQuestion.pack(side = TOP)
-	
-	#while nextButton:
-	#	pass
-
-	typeofans = "2"
-#while 
 
 page.mainloop()
