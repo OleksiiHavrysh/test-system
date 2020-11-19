ea21d916b8290552bc9947f4e31653013de14450
gavrysha <alexgavryshua@gmail.com>
2020-11-19 22:21:10 +0200

    Fix some radiobutton errors and add new functionality

--- TestSystem.py
+++ TestSystem.py
@@ -11,53 +11,79 @@ page.configure(bg = 'grey')
 number = int(file.readline().rstrip("\n"))
 typeofans = "1"
 flag = False
-j = 0
+
 atleastonce = False
 numofans = int(0)
 
+
+
+
+labelQuestion = Label(page, text = "", font = ('Helvetica', 14, 'bold'))
+
+rows, cols = (11, 11) 
+
+radiobuttons = [[]*cols]*rows
+checkbuttons = [[]*cols]*rows
+images = [[]*cols]*rows
+numRad = 0
+numCheck = 0
+numImg = 0
+
+prev = -1
+j = int(0)
+'''
+def Increm():
+    j -= 1
+'''
 nextButton = Button(page, text = "Следующий вопрос", background="#555", foreground="white")
+#, command = Increm()
 previousButton = Button(page, text = "Предыдущий вопрос", background="#555", foreground="white")
 nextButton.place(x = 300, y = 300)
 previousButton.place(x = 100, y = 300)
 
-
-labelQuestion = Label(page, text = "", font = ('Helvetica', 14, 'bold'))
 while j < number:
 
+    if j == prev:
+        j += 1
+        continue
+    else: 
+        prev = j
+        j += 1
+    active = IntVar();
+	#active.set(0);
+    numofans = int(file.readline().rstrip("\n"))
+    typeofans = str(file.readline().rstrip("\n"))
+    Question = str(file.readline().rstrip("\n"))
+    Question = Question.rstrip("\n")
+    i = 0
+    Answers = []
+    for i in range(numofans):
+	    Answers.append(str(file.readline().rstrip("\n")))
+	    Answers[i] = Answers[i].rstrip("\n")
 	
-	j += 1
+    i = 0
+    if typeofans == '1':
+        numRad = 0
+        for i in range(numofans):
+            numRad += 1
+            radiobuttons[j].append(Radiobutton(page, text = Answers[i], variable = active, value = j*10+numRad, font = ('Helvetica', 14)))
+            radiobuttons[j][i].place(x = 0, y = 50+i*250/numofans)
+    if typeofans == '2':
+        numCheck = 0
+        for i in range(numofans):
+            numCheck += 1
+            checkbuttons[j].append(Checkbutton(page, text = Answers[i], font = ('Helvetica', 14)))
+            checkbuttons[j][i].place(x = 0, y = 50+i*250/numofans)
+    if typeofans == '3':
+        numImg = 0
+        for i in range(numofans):
+            numImg += 1
+            images.append(ImageTk.PhotoImage(Image.open(Answers[i])))
+            panel = Label(page, image = images[i], width = 10, height = 10)
+            panel.pack(side = LEFT, fill = "both", expand = "yes")
 
-	active = IntVar();
-	active.set(0);
-	numofans = int(file.readline().rstrip("\n"))
-	typeofans = str(file.readline().rstrip("\n"))
-	Question = str(file.readline().rstrip("\n"))
-	Question = Question.rstrip("\n")
-	i = 0
-	Answers = []
-	for i in range(numofans):
-		Answers.append(str(file.readline().rstrip("\n")))
-		Answers[i] = Answers[i].rstrip("\n")
-	radiobuttons = []
-	checkbuttons = []
-	images = []
-	i = 0
-	if typeofans == '1':
-		for i in range(numofans):
-			radiobuttons.append(Radiobutton(page, text = Answers[i], variable = active, value = 0, font = ('Helvetica', 14)))
-			radiobuttons[i].place(x = 0, y = 50+i*250/numofans)
-	if typeofans == '2':
-		for i in range(numofans):
-			checkbuttons.append(Checkbutton(page, text = Answers[i], font = ('Helvetica', 14)))
-			checkbuttons[i].place(x = 0, y = 50+i*250/numofans)
-	if typeofans == '3':
-		for i in range(numofans):
-			images.append(ImageTk.PhotoImage(Image.open(Answers[i])))
-			panel = Label(page, image = images[i], width = 10, height = 10)
-			panel.pack(side = LEFT, fill = "both", expand = "yes")
+    labelQuestion.config(text=Question)
 
-	labelQuestion.config(text=Question)
-
-	labelQuestion.pack(side = TOP)
+    labelQuestion.pack(side = TOP)
 
 page.mainloop()
