96fe43805188fadf2a63c2f82cfb216ca1d06249
gavrysha <alexgavryshua@gmail.com>
2020-11-23 14:08:42 +0200

    Add ability to end test, partly debug indexes, add new file to compare answers

--- TestSystem.py
+++ TestSystem.py
@@ -1,8 +1,10 @@
 from tkinter import *
 from array import *
-from PIL import ImageTk, Image
+import random
+#from PIL import ImageTk, Image
 file = open("input.txt", 'r')
 #pip install pillow 
+trueans = open("trueans.txt", 'r')
 
 root = Tk()
 root.title("Тестирующая система")
@@ -28,27 +30,6 @@ rows, cols = (20, 11)
 prev = -1
 j = int(0)
 
-def Increm():
-    global j
-    j += 1
-    j = min (number, j)
-def Decrem():
-    global j
-    j -= 1
-    j = max (0, j)
-def EndTest():
-    global notend
-    if firststart == False:
-        notend = True
-nextButton = Button(root, text = "Следующий вопрос", background="#555", foreground="white", command = lambda: Increm())
-previousButton = Button(root, text = "Предыдущий вопрос", background="#555", foreground="white", command = lambda: Decrem())
-endButton = Button(root, text = "Завершить тест", background="#555", foreground="white", command = lambda: EndTest())
-nextButton.place(x = 320, y = 300)
-previousButton.place(x = 50, y = 300)
-endButton.place(x = 200, y = 300)
-
-
-
 Questions = []
 Answers = [[]*rows]*cols
 typeofans = []
@@ -59,14 +40,51 @@ def input_():
 		numofans.append(int(file.readline().rstrip("\n")))
 		typeofans.append(str(file.readline().rstrip("\n")))
 		Questions.append(str(file.readline().rstrip("\n")))
-    	
 		for j in range(numofans[i]):
-			Answers[i][j] = (str(file.readline().rstrip("\n")))
+			Answers[i].append(str(file.readline().rstrip("\n")))
 			#Answers[i][j] = Answers[i][j].rstrip("\n")
+                
+		#random.shuffle(Answers[i])
+	#random.shuffle(Answers)
 	notend = True
 
-	
 input_()
+
+def Increm():
+    global j
+    j += 1
+    j = min (number - 1, j)
+def Decrem():
+    global j
+    j -= 1
+    j = max (0, j)
+def EndTest():
+    global notend
+    if firststart == False:
+        notend = False
+        if typeofans[j] == '1':
+            for i in range(numofans[j]):
+                    radiobuttons[i].destroy()
+        if typeofans[j] == '2':
+            for i in range(numofans[j]):
+                    checkbuttons[i].destroy()
+
+        labelQuestion.destroy()
+        nextButton.destroy()
+        previousButton.destroy()
+        endButton.destroy()
+        
+        
+nextButton = Button(root, text = "Следующий вопрос", background="#555", foreground="white", command = lambda: Increm())
+previousButton = Button(root, text = "Предыдущий вопрос", background="#555", foreground="white", command = lambda: Decrem())
+endButton = Button(root, text = "Завершить тест", background="#555", foreground="white", command = lambda: EndTest())
+nextButton.place(x = 320, y = 300)
+previousButton.place(x = 50, y = 300)
+endButton.place(x = 200, y = 300)
+
+
+
+	
 
 notend == False
 j = 0
@@ -79,22 +97,26 @@ while notend:
     active = IntVar();
     i = 0
     if typeofans[j] == '1':
+        if not firststart:
+            for i in radiobuttons:
+                radiobuttons[i].destroy()
         for i in range(numofans[j]):
             radiobuttons.append(Radiobutton(root, text = Answers[j][i], variable = active, value = j*10+i, font = ('Helvetica', 14)))
             radiobuttons[i].place(x = 0, y = 50+i*250/numofans[j])
     if typeofans[j] == '2':
+        if not firststart:
+            for i in checkbuttons:
+                checkbuttons[i].destroy()
         for i in range(numofans[j]):
             checkbuttons.append(Checkbutton(root, text = Answers[j][i], font = ('Helvetica', 14)))
             checkbuttons[i].place(x = 0, y = 50+i*250/numofans[j])
-    if typeofans[j] == '3':
-        for i in range(numofans[j]):
-            images.append(ImageTk.PhotoImage(Image.open(Answers[j][i])))
-            panel = Label(root, image = images[i], width = 10, height = 10)
-            panel.pack(side = LEFT, fill = "both", expand = "yes")
+    #if typeofans[j] == '3':
+        #for i in range(numofans[j]):
+            #images.append(ImageTk.PhotoImage(Image.open(Answers[j][i])))
+            #panel = Label(root, image = images[i], width = 10, height = 10)
+            #panel.pack(side = LEFT, fill = "both", expand = "yes")
 
     labelQuestion.config(text=Questions[j])
-
     labelQuestion.pack(side = TOP)
     root.mainloop()
 
-#root.mainloop()
